4dae9b2cf84003124d63deb728caedc0201da375
arttail1 <arttail1@mail.ru>
2021-11-04 22:21:33 +0300

    Заменил switch на объект, проверка с помощью функций

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,37 @@
-function calc(operation, num1, num2) {
-
-    // Вариант проверки без доп. функций
-    // const isArgsUndefined = operation === undefined && num1 === undefined && num2 === undefined;
-    // const isOperandNaN = typeof num1 !== 'number' || typeof num2 !== 'number';
-    // const isNotValid = isArgsUndefined || isOperandNaN;
-    // const isDivisorZero = num2 === 0 && operation === 'div' || num2 === 0 && operation === 'rem';
-
+function calc(operator, num1, num2) {
     function isNotNumber(arg) {
         return (typeof arg !== 'number');
     }
-
-    function isUndefined(arg) {
-        return (arg === undefined);
+    function isDivisorZero() {
+        if (num2 === 0) return operator === 'div' || operator === 'rem';        
     }
-    const isNotValid = isUndefined(operation) && isUndefined(num1) && isUndefined(num2) || (isNotNumber(num1) || isNotNumber(num2));
-    const isDivisorZero = num2 === 0 && (operation === 'div' || operation === 'rem');
+    const mathActions = {
+        'sum': num1 + num2,
+        'diff': num1 - num2,
+        'mult': num1 * num2,
+        'div': num1 / num2,
+        'rem': num1 % num2,
+        'expo': num1 ** num2,
+    }
 
-    if (isNotValid) {
+    if (isNotNumber(num1) || isNotNumber(num2)) {
         return 'error';
-    } else if (isDivisorZero) {
+    } else if (isDivisorZero()) {
         return 'на ноль делить нельзя';
-    }
-    switch (operation) {
-        case 'sum':
-            return num1 + num2;
-        case 'diff':
-            return num1 - num2;
-        case 'mult':
-            return num1 * num2;
-        case 'div':
-            return num1 / num2;
-        case 'rem':
-            return num1 % num2;
-        case 'expo':
-            return num1 ** num2;
-        default:
-            return 'unknown operations';
-    }
+    } else if (!(operator in mathActions)) {
+        return 'unknown operations'
+    } return mathActions[operator];    
 }
 
 console.log(calc('sum', 5, 2));
-console.log(calc('diff', 4, 2));
+console.log(calc('diff', 5, 2));
 console.log(calc('mult', 5, 2));
+console.log(calc('div', 5, 2));
+console.log(calc('rem', 5, 2));
+console.log(calc('expo', 5, 3));
+console.log(calc('exp', 5, 3));
+console.log(calc('mult', 5, '2'));
 console.log(calc('div', 5, 0));
 console.log(calc('rem', 5, 0));
-console.log(calc('expso', 5, 13));
+console.log(calc());
+console.log(calc('divi', 5, 2));
